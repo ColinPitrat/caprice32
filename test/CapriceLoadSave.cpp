45bae69660fbefbf85a0c63387d994db3b27c56b
Colin Pitrat <colin.pitrat@gmail.com>
2016-07-28 22:39:13 +0100

    Cleaning

--- test/CapriceLoadSave.cpp
+++ test/CapriceLoadSave.cpp
@@ -33,10 +33,10 @@ TEST_F(CapriceLoadSaveTest, MatchCurrentFileSpecReturnsFalseIfExtensionListIsEmp
   std::list<std::string> fileSpec;
   SetFileSpec(fileSpec);
 
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test.zip"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test.dsk"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec(""));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test.zip"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test.dsk"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec(""));
 }
 
 TEST_F(CapriceLoadSaveTest, MatchCurrentFileSpecReturnsTrueIffExtensionInExtensionList)
@@ -44,13 +44,13 @@ TEST_F(CapriceLoadSaveTest, MatchCurrentFileSpecReturnsTrueIffExtensionInExtensi
   std::list<std::string> fileSpec = { ".zip", ".dsk" };
   SetFileSpec(fileSpec);
 
-  ASSERT_EQ(true, cls->MatchCurrentFileSpec("test.zip"));
-  ASSERT_EQ(true, cls->MatchCurrentFileSpec("test.dsk"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test.cpr"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test.voc"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("zip"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec(""));
+  ASSERT_TRUE(cls->MatchCurrentFileSpec("test.zip"));
+  ASSERT_TRUE(cls->MatchCurrentFileSpec("test.dsk"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test.cpr"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test.voc"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("zip"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec(""));
 }
 
 TEST_F(CapriceLoadSaveTest, MatchCurrentFileSpecSupportsVariousExtensionSizes)
@@ -58,11 +58,11 @@ TEST_F(CapriceLoadSaveTest, MatchCurrentFileSpecSupportsVariousExtensionSizes)
   std::list<std::string> fileSpec = { ".tar.gz", ".c", "nodot" };
   SetFileSpec(fileSpec);
 
-  ASSERT_EQ(true, cls->MatchCurrentFileSpec("test.tar.gz"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test.gz"));
-  ASSERT_EQ(true, cls->MatchCurrentFileSpec("test.c"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("test.cpp"));
-  ASSERT_EQ(true, cls->MatchCurrentFileSpec("nodot"));
-  ASSERT_EQ(true, cls->MatchCurrentFileSpec("test.nodot"));
-  ASSERT_EQ(false, cls->MatchCurrentFileSpec("dot"));
+  ASSERT_TRUE(cls->MatchCurrentFileSpec("test.tar.gz"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test.gz"));
+  ASSERT_TRUE(cls->MatchCurrentFileSpec("test.c"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("test.cpp"));
+  ASSERT_TRUE(cls->MatchCurrentFileSpec("nodot"));
+  ASSERT_TRUE(cls->MatchCurrentFileSpec("test.nodot"));
+  ASSERT_FALSE(cls->MatchCurrentFileSpec("dot"));
 }
