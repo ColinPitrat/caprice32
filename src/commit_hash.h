328b6a70368f534a245deba590c7a5ecfa715836
sebhz <seb@zyglute.fr>
2017-04-04 15:28:42 +0200

    Added MS Windows keymap

--- src/commit_hash.h
+++ src/commit_hash.h
@@ -1,6 +1,6 @@
 #ifndef COMMIT_HASH_H
 #define COMMIT_HASH_H
 
-std::string commit_hash = "dd1c6f7ace8df55225702baf931b34138c65f04f";
+std::string commit_hash = "8391fe4b190245754a57832a1b93d980e49f093e";
 
 #endif
