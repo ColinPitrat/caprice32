d20c63c1b5196a2234ef6d9f06f82f2a4b019a68
Colin Pitrat <colin.pitrat@gmail.com>
2019-05-10 09:55:55 +0100

    Investigate why diff fail on appveyor

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -10,6 +10,13 @@ touch "${LOGFILE}"
 
 $CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
+echo "Running diff printer.dat expected.dat"
+echo "Directory content:"
+ls
+echo "diff output:"
+diff printer.dat expected.dat
+echo "diff --help:"
+diff --help
 if diff printer.dat expected.dat >> "${LOGFILE}"
 then
   exit 0
