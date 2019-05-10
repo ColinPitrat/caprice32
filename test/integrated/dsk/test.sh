765e83214feff7664d3b1e5e927a45f83fbf73b8
Colin Pitrat <colin.pitrat@gmail.com>
2019-05-10 17:52:24 +0100

    Cleanup now that e2e_test works for windows

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -10,18 +10,6 @@ touch "${LOGFILE}"
 
 $CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
-echo "$DIFF printer.dat expected.dat:"
-$DIFF printer.dat expected.dat
-echo "$DIFF --help:"
-$DIFF --help
-
-echo "Not expected" > not_expected.dat
-for my_diff in /c/msys64/usr/lib/python2.7/site-packages/bzrlib/tests/test_patches_data/diff /c/msys64/usr/share/quilt/diff /c/cygwin/bin/diff.exe /c/cygwin64/bin/diff.exe /c/MinGW/msys/1.0/bin/diff.exe /c/msys64/usr/bin/diff.exe /c/msys64/usr/bin/svn-tools/diff.exe "/c/Program Files/Git/usr/bin/diff.exe" /c/projects/caprice32/msys64/usr/bin/diff.exe /c/Ruby193/DevKit/bin/diff.exe /c/Ruby23/DevKit/bin/diff.exe /c/Ruby23-x64/DevKit/bin/diff.exe
-do
-	echo " == $my_diff printer.dat not_expected.dat"
-	$my_diff printer.dat not_expected.dat
-done
-
 if $DIFF printer.dat expected.dat >> "${LOGFILE}"
 then
   exit 0
