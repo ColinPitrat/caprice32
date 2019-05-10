21a3e3b58968e93d60d199ab97876fdb0acb3773
Colin Pitrat <colin.pitrat@gmail.com>
2019-05-10 17:41:41 +0100

    Better way to find the working diff.exe

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -15,10 +15,11 @@ $DIFF printer.dat expected.dat
 echo "$DIFF --help:"
 $DIFF --help
 
-for my_diff in /c/msys64/usr/lib/python2.7/site-packages/bzrlib/tests/test_patches_data/diff /c/msys64/usr/share/quilt/diff /c/cygwin/bin/diff.exe /c/cygwin64/bin/diff.exe /c/MinGW/msys/1.0/bin/diff.exe /c/msys64/usr/bin/diff.exe /c/msys64/usr/bin/svn-tools/diff.exe /c/Program Files/Git/usr/bin/diff.exe /c/projects/caprice32/msys64/usr/bin/diff.exe /c/Ruby193/DevKit/bin/diff.exe /c/Ruby23/DevKit/bin/diff.exe /c/Ruby23-x64/DevKit/bin/diff.exe
+echo "Not expected" > not_expected.dat
+for my_diff in /c/msys64/usr/lib/python2.7/site-packages/bzrlib/tests/test_patches_data/diff /c/msys64/usr/share/quilt/diff /c/cygwin/bin/diff.exe /c/cygwin64/bin/diff.exe /c/MinGW/msys/1.0/bin/diff.exe /c/msys64/usr/bin/diff.exe /c/msys64/usr/bin/svn-tools/diff.exe "/c/Program Files/Git/usr/bin/diff.exe" /c/projects/caprice32/msys64/usr/bin/diff.exe /c/Ruby193/DevKit/bin/diff.exe /c/Ruby23/DevKit/bin/diff.exe /c/Ruby23-x64/DevKit/bin/diff.exe
 do
-	echo " == $my_diff printer.dat expected.dat"
-	$my_diff printer.dat expected.dat
+	echo " == $my_diff printer.dat not_expected.dat"
+	$my_diff printer.dat not_expected.dat
 done
 
 if $DIFF printer.dat expected.dat >> "${LOGFILE}"
