aa45369de7c0cdcd9e6401c3ff2bf43feffc8baa
Colin Pitrat <colin.pitrat@gmail.com>
2019-05-10 11:45:26 +0100

    Even more investigation

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -10,18 +10,23 @@ touch "${LOGFILE}"
 
 $CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
-echo "Running diff printer.dat expected.dat"
-echo "Directory content:"
-ls
-echo "which diff:"
-which diff
-echo "\\diff:"
-\diff printer.dat expected.dat
-echo "diff output:"
-diff printer.dat expected.dat
-echo "diff --help:"
-diff --help
-if diff printer.dat expected.dat >> "${LOGFILE}"
+DIFF=diff
+if [ -f "msys64/usr/bin/diff" ]
+then
+	DIFF=msys64/usr/bin/diff
+fi
+echo "msys64:"
+ls msys64
+echo "msys64/usr/bin:"
+ls msys64/usr/bin
+echo "msys64/usr/bin/diff:"
+ls -l msys64/usr/bin/diff
+echo "c:"
+ls c
+echo "c/Program Files:"
+ls "c/Program Files"
+
+if $DIFF printer.dat expected.dat >> "${LOGFILE}"
 then
   exit 0
 else
