c9bb6d58660363f64a4413574b0ecefca680c9e3
Colin Pitrat <colin.pitrat@gmail.com>
2019-05-10 09:44:00 +0100

    Cleanup

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -8,7 +8,6 @@ export SDL_VIDEODRIVER=dummy
 cd "$TSTDIR"
 touch "${LOGFILE}"
 
-# The CLS is here mostly to let time to the program to run
 $CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
 if diff printer.dat expected.dat >> "${LOGFILE}"
--- test/integrated/scripted_operations_timing/test.sh
+++ test/integrated/scripted_operations_timing/test.sh
@@ -4,7 +4,6 @@ TSTDIR=`dirname "$0"`
 LOGFILE="test.log"
 CAP32DIR="$TSTDIR/../../../"
 
-#export SDL_VIDEODRIVER=dummy
 cd "$TSTDIR"
 touch "${LOGFILE}"
 
@@ -14,13 +13,10 @@ rm -rv output
 mkdir -p output
 
 $CAP32DIR/cap32 -c cap32.cfg -a 'border 13:ink 0,13:ink 1,0:mode 1:for a=1 to 24:print"Hello World",a:next:call &bd19:call 0' -a 'CAP32_WAITBREAKCAP32_SCRNSHOT CAP32_EXIT'
-
-# Intended test when ready:
+# Intended test when ready (doesn't work for now because \n are added automatically at the end of -a):
 # $CAP32DIR/cap32 -c cap32.cfg -a 'border 13:ink 0,13:ink 1,0:mode 1:for a=1 to 24:print"Hello World",a:next:call &bd19:call 0' -a CAP32_WAITBREAK -a CAP32_SCRNSHOT -a CAP32_EXIT
-# Artifcially pass test until code is correct:
-# $CAP32DIR/cap32 -c cap32.cfg -a 'border 13:ink 0,13:ink 1,0:mode 1:for a=1 to 24:print"Hello World",a:next:call &bd19:poke 0,&c9:call 0:cat' -a CAP32_WAITBREAK -a CAP32_SCRNSHOT -a CAP32_EXIT
 
-# Screenshot file name is not reproducible (a features, not a bug), TODO have upstream code offer an option?
+# Screenshot file name is not predictible (a feature, not a bug)
 mv -v output/screenshot_*png output/screenshot.png
 
 if diff -ur model output >> "${LOGFILE}"
