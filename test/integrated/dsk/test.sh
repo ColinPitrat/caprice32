60b3031a91cc2bdecad52c24cbdbbafd4516cb41
Colin Pitrat <colin.pitrat@gmail.com>
2019-05-13 10:26:37 +0100

    Investigate why win32 e2e_test fails when win64 ones succeed

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -10,6 +10,17 @@ touch "${LOGFILE}"
 
 $CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
+echo "cap32 in TSTDIR:"
+find $TSTDIR | grep cap32
+
+echo "CAP32DIR: $CAP32DIR"
+echo "TSTDIR: $TSTDIR"
+echo "CAP32DIR content:"
+ls $CAP32DIR
+echo "TSTDIR content:"
+ls $TSTDIR
+
+
 if $DIFF printer.dat expected.dat >> "${LOGFILE}"
 then
   exit 0
