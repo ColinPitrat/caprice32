1689765d8ee629cc8c3641df5bcd8afc5e7e4193
Colin Pitrat <colin.pitrat@gmail.com>
2019-09-01 12:09:41 +0100

    Add description

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
+# Package description for Nix (http://nixos.org)
 with import <nixpkgs> {};
 caprice32.overrideDerivation (old: {
   # overrideDerivation allows it to specify additional dependencies
