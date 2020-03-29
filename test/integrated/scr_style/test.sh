525473c82832b28f4f521a4f9946023d7acace51
Colin Pitrat <colin.pitrat@gmail.com>
2020-03-29 23:41:27 +0100

    Use number of video plugins from ./cap32 -V output to test all plugins

--- test/integrated/scr_style/test.sh
+++ test/integrated/scr_style/test.sh
@@ -17,18 +17,25 @@ echo "" > "${LOGFILE}"
 
 cd "$TSTDIR"
 
-# TODO: Find a way to test the GL mode if built with
-for style in `seq 0 10`
+nb_plugins=`$CAP32DIR/cap32 -V | grep "Number of video plugins available: " | cut -d : -f 2 | xargs`
+let last_plugin=${nb_plugins}-1
+
+rc=0
+for style in `seq 0 $last_plugin`
 do
   sed -i "s/^scr_style=.*$/scr_style=${style}/" cap32.cfg
   $CAP32DIR/cap32 -c cap32.cfg -a "print #8,\"Hello, scr_style=${style}!\"" -a CAP32_EXIT >> "${LOGFILE}" 2>&1
-  mv output/printer.dat output/printer.dat.${style}
+
+  mv ${OUTPUT_DIR}/printer.dat ${OUTPUT_DIR}/printer.dat.${style}
+  if ! $DIFF ${OUTPUT_DIR}/printer.dat.${style} model/printer.dat.${style} >> "${LOGFILE}"
+  then
+    echo "!! Test failed for scr_style=${style}"
+    rc=1
+  fi
 done
 
-if $DIFF -ur model ${OUTPUT_DIR} >> "${LOGFILE}"
+if $rc -ne 0
 then
-  exit 0
-else
   cat "${LOGFILE}"
-  exit 1
 fi
+exit $rc
