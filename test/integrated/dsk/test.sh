1856547f3ee1f221179fca5f08b4ce380217357f
Colin Pitrat <colin.pitrat@gmail.com>
2020-03-29 17:13:50 +0100

    More homogeneous way to handle integrated tests

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 TSTDIR=`dirname "$0"`
+OUTPUT_DIR="output"
 LOGFILE="test.log"
-CAP32DIR="$TSTDIR/../../../"
+CAP32DIR="${TSTDIR}/../../../"
 
-export SDL_VIDEODRIVER=dummy
-cd "$TSTDIR"
+rm -rv ${OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
 touch "${LOGFILE}"
+
+cd "$TSTDIR"
+export SDL_VIDEODRIVER=dummy
 
 $CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
-if $DIFF printer.dat expected.dat >> "${LOGFILE}"
+if $DIFF output/printer.dat expected.dat >> "${LOGFILE}"
 then
   exit 0
 else
--- test/integrated/scripted_operations_timing/test.sh
+++ test/integrated/scripted_operations_timing/test.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 TSTDIR=`dirname "$0"`
+OUTPUT_DIR="output"
 LOGFILE="test.log"
-CAP32DIR="$TSTDIR/../../../"
+CAP32DIR="${TSTDIR}/../../../"
 
-cd "$TSTDIR"
+rm -rv ${OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
 touch "${LOGFILE}"
-
 find . -iname "*~" -exec rm -fv {} \;
 
-rm -rv output
-mkdir -p output
+cd "$TSTDIR"
+
 
 $CAP32DIR/cap32 -c cap32.cfg -a 'border 13:ink 0,13:ink 1,0:mode 1:for a=1 to 24:print"Hello World",a:next:call &bd19:call 0' -a 'CAP32_WAITBREAKCAP32_SCRNSHOT CAP32_EXIT'
 # Intended test when ready (doesn't work for now because \n are added automatically at the end of -a):
 # $CAP32DIR/cap32 -c cap32.cfg -a 'border 13:ink 0,13:ink 1,0:mode 1:for a=1 to 24:print"Hello World",a:next:call &bd19:call 0' -a CAP32_WAITBREAK -a CAP32_SCRNSHOT -a CAP32_EXIT
 
 # Screenshot file name is not predictible (a feature, not a bug)
-mv -v output/screenshot_*png output/screenshot.png
+mv -v ${OUTPUT_DIR}/screenshot_*png ${OUTPUT_DIR}/screenshot.png
 
-if $DIFF -ur model output >> "${LOGFILE}"
+if $DIFF -ur model ${OUTPUT_DIR} >> "${LOGFILE}"
 then
   exit 0
 else
