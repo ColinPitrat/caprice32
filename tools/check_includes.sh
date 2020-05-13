a05bc699f01a0d15a891648378c4ac756aba7ab9
Colin Pitrat <colin.pitrat@gmail.com>
2020-05-13 13:16:33 +0100

    Extend check_includes to map (and make it easy to extend to more)

--- tools/check_includes.sh
+++ tools/check_includes.sh
@@ -3,15 +3,24 @@
 uses="$(mktemp)"
 includes="$(mktemp)"
 
-token="std::string"
-header="string"
-grep -rlI "${token}" src/ test/ | sort -u > ${uses}
-grep -rlI "include <${header}>" src/ test/ | sort -u > ${includes}
+declare -A token_to_header
+# TODO: Extend this to vector, list and more ...
+token_to_header=( ["std::string"]="string" ["std::map"]="map" )
 
-errors=$(join -v 1 "${uses}" "${includes}" | wc -l)
-join -v 1 "${uses}" "${includes}" | while read line
+errors=0
+for token in "${!token_to_header[@]}"
 do
-  echo "${line} uses ${token} but doesn't include ${include}"
+  header=${token_to_header[${token}]}
+  echo "Looking for ${token} and ${header}"
+  grep -rlI "${token}" src/ test/ | sort -u > ${uses}
+  grep -rlI "include <${header}>" src/ test/ | sort -u > ${includes}
+
+  this_errors=$(join -v 1 "${uses}" "${includes}" | wc -l)
+  let errors=${errors}+${this_errors}
+  join -v 1 "${uses}" "${includes}" | while read line
+  do
+    echo "${line} uses ${token} but doesn't include ${header}"
+  done
 done
 
 exit ${errors}
