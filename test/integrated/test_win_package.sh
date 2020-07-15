6d534f5624b4e5d80e084d10c01c8bc2e7e9c22a
Colin Pitrat <colin.pitrat@gmail.com>
2020-07-15 14:27:49 +0100

    Give up on testing for missing DLL (for now)

--- test/integrated/test_win_package.sh
+++ test/integrated/test_win_package.sh
@@ -1,3 +1,7 @@
+# The idea of this test is to detect when DLLs are missing from the Windows
+# release.
+# TODO: Find a way to make this work. On AppVeyor, this never fails because the
+# DLLs are found (probably in the system directory)
 ARCH=$1
 TSTDIR=$(mktemp -d)
 LOGFILE=$(mktemp)
@@ -8,8 +12,7 @@ echo -e 'Hello, world!\r' > ${EXPECTED}
 cp -r release/cap32-${ARCH} ${TSTDIR}/
 cd ${TSTDIR}/cap32-${ARCH}
 sed -i 's/printer=0/printer=1/' cap32.cfg
-echo "$WINEPATH"
-wine ./cap32.exe -a "print #8,\"Hello, world!\"" -a CAP32_EXIT >> "${LOGFILE}" 2>&1
+./cap32.exe -a "print #8,\"Hello, world!\"" -a CAP32_EXIT >> "${LOGFILE}" 2>&1
 
 if ! diff ${EXPECTED} printer.dat
 then
