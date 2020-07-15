bd54872bfa094dc347cc857b83eda97106c01a64
Colin Pitrat <colin.pitrat@gmail.com>
2020-07-15 11:51:43 +0100

    Explicitly call wine and print out winepath

--- test/integrated/test_win_package.sh
+++ test/integrated/test_win_package.sh
@@ -8,7 +8,8 @@ echo -e 'Hello, world!\r' > ${EXPECTED}
 cp -r release/cap32-${ARCH} ${TSTDIR}/
 cd ${TSTDIR}/cap32-${ARCH}
 sed -i 's/printer=0/printer=1/' cap32.cfg
-./cap32.exe -a "print #8,\"Hello, world!\"" -a CAP32_EXIT >> "${LOGFILE}" 2>&1
+echo "$WINEPATH"
+wine ./cap32.exe -a "print #8,\"Hello, world!\"" -a CAP32_EXIT >> "${LOGFILE}" 2>&1
 
 if ! diff ${EXPECTED} printer.dat
 then
