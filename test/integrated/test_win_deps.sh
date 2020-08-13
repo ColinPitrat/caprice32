fec59fcc23983ea6cc438fda7b1b0836fcd89458
Colin Pitrat <colin.pitrat@gmail.com>
2020-08-13 13:21:00 +0100

    Try ldd. #174

--- test/integrated/test_win_deps.sh
+++ test/integrated/test_win_deps.sh
@@ -8,13 +8,15 @@ cat << EOF > ${EXPECTED}
 I don't know yet
 EOF
 
-dumpbin /dependents release/cap32-${ARCH} > ${ACTUAL}
+ldd release/cap32-${ARCH} > ${ACTUAL}
+
+cat ${ACTUAL}
 
 if ! diff ${ACTUAL} ${EXPECTED}
 then
   echo "!! package test for ${ARCH} failed"
   cat ${LOGFILE}
-  exit 1
+  #exit 1
 else
   echo "The test was successful!"
   cat printer.dat
