1e41dbd9d03d14e2c2c818436cafd842b55d51b1
Colin Pitrat <colin.pitrat@gmail.com>
2021-03-18 13:55:09 +0000

    Add an assertion to ensure the rects fit in the corresponding surfaces. #182

--- test/video.cpp
+++ test/video.cpp
@@ -25,6 +25,11 @@ class ComputeRectsTest : public testing::Test {
       EXPECT_EQ(r->h, s->h);
     }
 
+    void ExpectRectInSurface(SDL_Rect *r, SDL_Surface *s) {
+      EXPECT_LE(r->x + r->w, s->w);
+      EXPECT_LE(r->y + r->h, s->h);
+    }
+
     SDL_Surface* CreateSurface(int width, int height) {
       // BPP shouldn't influence this method. Anyway, it is currently called only by filters that support only 16bpp.
       return SDL_CreateRGBSurface(SDL_SWSURFACE, width, height, 16, 0, 0, 0, 0);
@@ -42,6 +47,8 @@ TEST_F(ComputeRectsTest, DefaultSize)
 
   ExpectFullSrc();
   ExpectRectMatchesSurface(&dst, vid);
+  ExpectRectInSurface(&src, pub);
+  ExpectRectInSurface(&dst, vid);
 }
 
 TEST_F(ComputeRectsTest, BiggerVid)
@@ -58,6 +65,8 @@ TEST_F(ComputeRectsTest, BiggerVid)
     EXPECT_EQ(dst.y, offset/2);
     EXPECT_EQ(dst.w, 2*CPC_VISIBLE_SCR_WIDTH);
     EXPECT_EQ(dst.h, 2*CPC_VISIBLE_SCR_HEIGHT);
+    ExpectRectInSurface(&src, pub);
+    ExpectRectInSurface(&dst, vid);
   }
 }
 
@@ -76,6 +85,8 @@ TEST_F(ComputeRectsTest, BiggerPub)
     // TODO: There is obviously a problem if offset/2 < 4 compared to when offset = 0 (where we have -4 here)
     EXPECT_EQ(src.h, CPC_VISIBLE_SCR_HEIGHT - offset/2);
     ExpectRectMatchesSurface(&dst, vid);
+    ExpectRectInSurface(&src, pub);
+    ExpectRectInSurface(&dst, vid);
   }
 }
 
