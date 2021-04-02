85c5f531c96839594ea32fef157d3cc366fc8110
Colin Pitrat <colin.pitrat@gmail.com>
2021-04-02 22:52:19 +0100

    Show output of cap32 no matter what to be able to make the difference between passing tests and failing ones on s390x

--- test/integrated/scr_style/test.sh
+++ test/integrated/scr_style/test.sh
@@ -24,7 +24,7 @@ rc=0
 for style in `seq 0 $last_plugin`
 do
   sed -i "s/^scr_style=.*$/scr_style=${style}/" cap32.cfg
-  $CAP32DIR/cap32 -c cap32.cfg -a "print #8,\"style=${style}\"" -a CAP32_EXIT >> "${LOGFILE}" 2>&1
+  $CAP32DIR/cap32 -c cap32.cfg -a "print #8,\"style=${style}\"" -a CAP32_EXIT
 
   mv ${OUTPUT_DIR}/printer.dat ${OUTPUT_DIR}/printer.dat.${style}
   if ! $DIFF ${OUTPUT_DIR}/printer.dat.${style} model/printer.dat.${style} >> "${LOGFILE}"
