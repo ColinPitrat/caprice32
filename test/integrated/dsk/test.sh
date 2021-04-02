4393978f8f456ae6660be51b46bdc6b3c33bf484
Colin Pitrat <colin.pitrat@gmail.com>
2021-04-02 22:42:15 +0100

    OK so it's not screenshots

--- test/integrated/dsk/test.sh
+++ test/integrated/dsk/test.sh
@@ -12,8 +12,7 @@ echo "" > "${LOGFILE}"
 cd "$TSTDIR"
 export SDL_VIDEODRIVER=dummy
 
-# TODO: Remove CAP32_SCRNSHOT after verifying if it makes the test crash on s390x
-$CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_SCRNSHOT -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
+$CAP32DIR/cap32 -c cap32.cfg -a "run\"hello" -a "call 0" -a CAP32_WAITBREAK -a CAP32_EXIT hello.zip >> "${LOGFILE}" 2>&1
 
 if $DIFF output/printer.dat expected.dat >> "${LOGFILE}"
 then
