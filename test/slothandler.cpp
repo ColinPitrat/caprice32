35ddfcff21dd81785dde4b93e270b107f3e4fa7a
Colin Pitrat <colin.pitrat@gmail.com>
2025-03-22 20:49:03 +0000

    Add tests for loading multiple disks from a single zip and switching between them. #21

--- test/slothandler.cpp
+++ test/slothandler.cpp
@@ -4,6 +4,21 @@
 #include "slotshandler.h"
 #include <string>
 
+extern t_drive driveA;
+extern t_drive driveB;
+extern byte* pbGPBuffer;
+
+TEST(SlotHandlerTest, slotsInitializedWithProperDriveTypes)
+{
+  t_CPC CPC;
+
+  ASSERT_EQ(DRIVE::DSK_A, CPC.driveA.drive);
+  ASSERT_EQ(DRIVE::DSK_B, CPC.driveB.drive);
+  ASSERT_EQ(DRIVE::TAPE, CPC.tape.drive);
+  ASSERT_EQ(DRIVE::SNAPSHOT, CPC.snapshot.drive);
+  ASSERT_EQ(DRIVE::CARTRIDGE, CPC.cartridge.drive);
+}
+
 TEST(SlotHandlerTest, fillSlotsNoArg)
 {
   std::vector<std::string> slot_list;
@@ -147,6 +162,64 @@ TEST(SlotHandlerTest, fillSlotsOneZippedCprFile)
   ASSERT_EQ("", CPC.tape.file);
   ASSERT_EQ("", CPC.snap_path);
   ASSERT_EQ("", CPC.snapshot.file);
+}
+
+unsigned int checksum(const t_drive* drive)
+{
+  unsigned int checksum = 0;
+  for (unsigned int track = 0; track < drive->tracks; track++) {
+    for (unsigned int side = 0; side <= drive->sides; side++) {
+      for (unsigned int byte = 0; byte < drive->track[track][side].size; byte++) {
+        checksum += drive->track[track][side].data[byte];
+      }
+    }
+  }
+  return checksum;
+}
+
+TEST(SlotHandlerTest, fillSlotsDiskAZipMultipleFiles)
+{
+  std::vector<std::string> slot_list = { "test/zip/test1.zip"};
+  t_CPC CPC;
+  // TODO: Replace pbGPBuffer by some dynamic object (e.g. vector), at least in dsk_load.
+  pbGPBuffer = new byte [128*1024];
+
+  fillSlots(slot_list, CPC);
+
+  ASSERT_EQ("", CPC.cart_path);
+  ASSERT_EQ("", CPC.cartridge.file);
+  ASSERT_EQ("", CPC.dsk_path);
+  ASSERT_EQ("test/zip/test1.zip", CPC.driveA.file);
+  ASSERT_EQ("", CPC.driveB.file);
+  ASSERT_EQ("", CPC.tape_path);
+  ASSERT_EQ("", CPC.tape.file);
+  ASSERT_EQ("", CPC.snap_path);
+  ASSERT_EQ("", CPC.snapshot.file);
+
+  // The zip contains 2 files.
+  // First file is empty.dsk
+  file_load(CPC.driveA);
+  auto checksum1 = checksum(&driveA);
+  // Second file is hello.dsk
+  CPC.driveA.zip_index += 1;
+  file_load(CPC.driveA);
+  auto checksum2 = checksum(&driveA);
+  // Switching again should loop back to empty.dsk
+  CPC.driveA.zip_index += 1;
+  file_load(CPC.driveA);
+  auto checksum3 = checksum(&driveA);
+  // And back to hello.dsk
+  CPC.driveA.zip_index += 1;
+  file_load(CPC.driveA);
+  auto checksum4 = checksum(&driveA);
+
+  // 1 and 3 are both "empty.dsk" so they are equals
+  // 2 and 4 are both "hello.dsk" so they are equals
+  ASSERT_EQ(checksum1, checksum3);
+  ASSERT_EQ(checksum2, checksum4);
+
+  // 1 and 2 are "empty.dsk" and "hello.dsk" so they are different
+  ASSERT_NE(checksum1, checksum2);
 }
 
 TEST(SlotHandlerTest, fillSlotsOneFileOfEachKind)
