ColinPitrat/caprice32

commit f0f128d122986fb5fa9bdd83d4299ec482eb1fa5

Fix display of sprites that are partially out of screen (left or top, i.e. negative coordinates)

src/asic.cpp
@@ -251,30 +251,28 @@ bool asic_register_page_write(word addr, byte val) {
          case 0:
             // X position
             asic.sprites_x[id] = (asic.sprites_x[id] & 0xFF00) | val;
-            //LOG_DEBUG("Received sprite X for sprite " << id << " x=" << asic.sprites_x[id]);
+            //LOG_DEBUG("Received sprite X (LSB) for sprite " << id << " x=" << asic.sprites_x[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;
          case 1:
             // X position
-            val = val & 0x3;
             asic.sprites_x[id] = (asic.sprites_x[id] & 0x00FF) | (val << 8);
-            //LOG_DEBUG("Received sprite X for sprite " << id << " x=" << asic.sprites_x[id]);
+            //LOG_DEBUG("Received sprite X (MSB) for sprite " << id << " x=" << asic.sprites_x[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;
          case 2:
             // Y position
             asic.sprites_y[id] = ((asic.sprites_y[id] & 0xFF00) | val);
-            //LOG_DEBUG("Received sprite Y for sprite " << id << " y=" << asic.sprites_y[id]);
+            //LOG_DEBUG("Received sprite Y (LSB) for sprite " << id << " y=" << asic.sprites_y[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;
          case 3:
             // Y position
-            val = val & 0x1;
             asic.sprites_y[id] = ((asic.sprites_y[id] & 0x00FF) | (val << 8));
-            //LOG_DEBUG("Received sprite Y for sprite " << id << " y=" << asic.sprites_y[id]);
+            //LOG_DEBUG("Received sprite Y (MSB) for sprite " << id << " y=" << asic.sprites_y[id]);
             // Affect RAM image
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;

commit 546e1e1c6b61ba840d5f36924f03b25428cf4c1b

Fix sprite handling that was broken by f0f128d122986fb5fa9bdd83d4299ec482eb1fa5 - negative values are values above the maximum positive value (767 for x, 255 for y)

src/asic.cpp
@@ -281,32 +281,55 @@ bool asic_register_page_write(word addr, byte val) {
       int id = ((addr - 0x6000) >> 3);
       int type = (addr & 0x7);
       switch (type) {
+         // From Arnold V5 specs:
+         // Data written to these registers should be between +767 and -256 for X, and between +255 and -256 for Y.
+         // With standard 6845 timing (64us scan lines, 200 visible lines), "on screen" positions at maximum sprite magnification are -64 to +639 in x and -63 to +199 in y.
+         // A sprite will not be displayed if either the vertical or the horizontal positions outside the on screen range.
          case 0:
             // X position
-            asic.sprites_x[id] = (asic.sprites_x[id] & 0xFF00) | val;
-            //LOG_DEBUG("Received sprite X (LSB) for sprite " << id << " x=" << asic.sprites_x[id]);
+            {
+               uint16_t x = (static_cast<uint16_t>(asic.sprites_x[id]) & 0xFF00) | val;
+               x &= 0x03FF;
+               if (x > 767) x |= 0xFC00; // Sign-extend bit 10
+               asic.sprites_x[id] = static_cast<int16_t>(x);
+            }
+            LOG_DEBUG("Received sprite X (LSB=" << static_cast<uint16_t>(val) << ") for sprite " << id << " x=" << asic.sprites_x[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;
          case 1:
             // X position
-            asic.sprites_x[id] = (asic.sprites_x[id] & 0x00FF) | (val << 8);
-            //LOG_DEBUG("Received sprite X (MSB) for sprite " << id << " x=" << asic.sprites_x[id]);
+            {
+               uint16_t x = (static_cast<uint16_t>(asic.sprites_x[id]) & 0x00FF) | (val << 8);
+               x &= 0x03FF;
+               if (x > 767) x |= 0xFC00; // Sign-extend bit 10
+               asic.sprites_x[id] = static_cast<int16_t>(x);
+            }
+            LOG_DEBUG("Received sprite X (MSB=" << static_cast<uint16_t>(val) << ") for sprite " << id << " x=" << asic.sprites_x[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;
          case 2:
             // Y position
-            asic.sprites_y[id] = ((asic.sprites_y[id] & 0xFF00) | val);
-            //LOG_DEBUG("Received sprite Y (LSB) for sprite " << id << " y=" << asic.sprites_y[id]);
+            {
+               uint16_t y = (static_cast<uint16_t>(asic.sprites_y[id]) & 0xFF00) | val;
+               y &= 0x01FF;
+               if (y > 255) y |= 0xFE00; // Sign-extend bit 8
+               asic.sprites_y[id] = static_cast<int16_t>(y);
+            }
+            LOG_DEBUG("Received sprite Y (LSB=" << static_cast<uint16_t>(val) << ") for sprite " << id << " y=" << asic.sprites_y[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;
          case 3:
             // Y position
-            asic.sprites_y[id] = ((asic.sprites_y[id] & 0x00FF) | (val << 8));
-            //LOG_DEBUG("Received sprite Y (MSB) for sprite " << id << " y=" << asic.sprites_y[id]);
-            // Affect RAM image
+            {
+               uint16_t y = (static_cast<uint16_t>(asic.sprites_y[id]) & 0x00FF) | (val << 8);
+               y &= 0x01FF;
+               if (y > 255) y |= 0xFE00; // Sign-extend bit 8
+               asic.sprites_y[id] = static_cast<int16_t>(y);
+            }
+            LOG_DEBUG("Received sprite Y (MSB=" << static_cast<uint16_t>(val) << ") for sprite " << id << " y=" << asic.sprites_y[id]);
             // Mirrored in RAM image 4 bytes after
             pbRegisterPage[(addr & 0x3FFF) + 4] = val;
             break;

src/crtc.cpp
@@ -674,7 +674,7 @@ inline void match_hsw()
          GateArray.sl_count = 0; // clear counter
       }
       if (CRTC.sl_count == CRTC.interrupt_sl && CRTC.interrupt_sl != 0) {
-         LOG_INFO("Firing PRI interrupt at GA.sl_count=" << static_cast<int>(GateArray.sl_count) << ", CRTC.sl_count=" << CRTC.sl_count);
+         LOG_DEBUG("Firing PRI interrupt at GA.sl_count=" << static_cast<int>(GateArray.sl_count) << ", CRTC.sl_count=" << CRTC.sl_count);
          z80.int_pending = 1;
          asic.raster_int_pending = true;
          // Set the bit for the raster interrupt in DCSR.

test/asic.cpp
@@ -7,6 +7,7 @@
 extern byte *membank_config[8][4];
 extern byte *membank_write[4];
 extern t_GateArray GateArray;
+extern byte *pbRegisterPage;
 
 namespace 
 {
@@ -197,6 +198,79 @@ TEST_F(AsicTest, AsicDMACycleWriteBackDCSR)
 
   EXPECT_EQ(2, *(membank_write[1] + 0x2c00));
   EXPECT_EQ(1, *(membank_write[1] + 0x2c0f));
+}
+
+TEST_F(AsicTest, SpriteCoordinatesSignExtension)
+{
+   std::vector<byte> registerPage(16 * 1024, 0);
+   pbRegisterPage = registerPage.data();
+
+   // Clear sprites x and y coordinates
+   for (int i = 0; i < 16; ++i) {
+      asic.sprites_x[i] = 0;
+      asic.sprites_y[i] = 0;
+   }
+
+   //////
+   // Test X coordinate parsing (id = 0)
+   //////
+
+   // Positive X coordinate below 512
+   // 350 = 0x015E
+   asic_register_page_write(0x6000, 0x5E);
+   asic_register_page_write(0x6001, 0x01);
+   EXPECT_EQ(350, asic.sprites_x[0]);
+
+   // Positive X coordinate above 512 but below 768
+   // 640 = 0x0280
+   asic_register_page_write(0x6000, 0x80);
+   asic_register_page_write(0x6001, 0x02);
+   EXPECT_EQ(640, asic.sprites_x[0]);
+
+   // 767 = 0x02FF
+   asic_register_page_write(0x6000, 0xFF);
+   asic_register_page_write(0x6001, 0x02);
+   EXPECT_EQ(767, asic.sprites_x[0]);
+
+   // Negative X coordinate >= 768
+   // 768 = 0x0300 (= -256)
+   asic_register_page_write(0x6000, 0x00);
+   asic_register_page_write(0x6001, 0x03);
+   EXPECT_EQ(-256, asic.sprites_x[0]);
+
+   // 1000 = 0x03E8 (= -24)
+   asic_register_page_write(0x6000, 0xE8);
+   asic_register_page_write(0x6001, 0x03);
+   EXPECT_EQ(-24, asic.sprites_x[0]);
+
+
+   //////
+   // Test Y coordinate parsing (id = 0)
+   //////
+
+   // Positive Y coordinate below 256
+   // 200 = 0x00C8
+   asic_register_page_write(0x6002, 0xC8);
+   asic_register_page_write(0x6003, 0x00);
+   EXPECT_EQ(200, asic.sprites_y[0]);
+
+   // 255 = 0x00FF
+   asic_register_page_write(0x6002, 0xFF);
+   asic_register_page_write(0x6003, 0x00);
+   EXPECT_EQ(255, asic.sprites_y[0]);
+
+   // Negative Y coordinate >= 256
+   // 256 = 0x0100 (= -256)
+   asic_register_page_write(0x6002, 0x00);
+   asic_register_page_write(0x6003, 0x01);
+   EXPECT_EQ(-256, asic.sprites_y[0]);
+
+   // 500 = 0x01F4 (= -12)
+   asic_register_page_write(0x6002, 0xF4);
+   asic_register_page_write(0x6003, 0x01);
+   EXPECT_EQ(-12, asic.sprites_y[0]);
+
+   pbRegisterPage = nullptr;
 }
 
 }